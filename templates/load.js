4fd5a7393def6fc8bf34452a7884b7ed00bbccf5
AgentTroll <woodyc40@gmail.com>
2014-07-22 16:37:28 -0700

    Update load.js

--- templates/load.js
+++ templates/load.js
@@ -1,13 +1,3 @@
-var loadPage = function (href) {
-  'use strict';
-  var xmlhttp = new XMLHttpRequest();
-  
-  xmlhttp.open("GET", href, false);
-  xmlhttp.send();
-
-  return xmlhttp.responseText;
-};
-
 var load = function (doc) {
   'use strict';
   var xmlhttp = new XMLHttpRequest();
