8419632180d87fb860b973c4caa61e8bba705b55
AgentTroll <woodyc40@gmail.com>
2014-07-22 16:25:37 -0700

    Update load.js

--- templates/load.js
+++ templates/load.js
@@ -1,13 +1,16 @@
-var loadPage = function(href) {
+var loadPage = function (href) {
+  'use strict';
   var xmlhttp = new XMLHttpRequest();
- 
+  
   xmlhttp.open("GET", href, false);
   xmlhttp.send();
- 
+
   return xmlhttp.responseText;
 };
 
-var load = function(doc) {
+var load = function (doc) {
+  'use strict';
+  
   doc.getElementById("navbar").innerHTML = loadPage("/templates/navbar.html");
   doc.getElementById("footer").innerHTML = loadPage("/templates/footer.html");
 };
