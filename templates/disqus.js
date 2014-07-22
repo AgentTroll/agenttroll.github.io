f0bb98cf826e41f01e13f14975f4128a301e4426
AgentTroll <woodyc40@gmail.com>
2014-07-21 22:40:11 -0700

    Update disqus.js

--- templates/disqus.js
+++ templates/disqus.js
@@ -1,6 +1,6 @@
 var disqus_shortname = 'agenttrolldude';
       
-function(doc) {
+function disqus(doc) {
   var dsq = doc.createElement('script'); dsq.type = 'text/javascript'; dsq.async = true;
   dsq.src = '//' + disqus_shortname + '.disqus.com/embed.js';
   (doc.getElementsByTagName('head')[0] || doc.getElementsByTagName('body')[0]).appendChild(dsq);
