c5f372a67e1fbdcc75cf5324792a4fd9f6a81c00
AgentTroll <woodyc40@gmail.com>
2014-07-21 22:32:40 -0700

    Create disqus.js

---
+++ templates/disqus.js
@@ -0,0 +1,7 @@
+var disqus_shortname = 'agenttrolldude';
+      
+function(doc) {
+  var dsq = doc.createElement('script'); dsq.type = 'text/javascript'; dsq.async = true;
+  dsq.src = '//' + disqus_shortname + '.disqus.com/embed.js';
+  (doc.getElementsByTagName('head')[0] || doc.getElementsByTagName('body')[0]).appendChild(dsq);
+}
